ff128b0b3ef452b5f83f2bcc8177b071c8fdd5cd
linus-cl <linuscl2002@gmail.com>
2024-04-19 15:58:00 +0200

    added button

--- main.py
+++ main.py
@@ -3,44 +3,57 @@ import pygame
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1200, 900))
+width = screen.get_width()
+height = screen.get_height()
 clock = pygame.time.Clock()
 running = True
 dt = 0
+font = pygame.font.SysFont("Sans", 20)
 
-table_board = pygame.Rect(
-    50, 50, (screen.get_width() / 2) + 50 / 2, screen.get_height() - 100
-)
+table_board = pygame.Rect(50, 50, (width / 2) + 50 / 2, height - 100)
 dice_board = pygame.Rect(
-    (screen.get_width() / 2) + 100,
+    (width / 2) + 100,
     150,
-    (screen.get_width() - 300) / 2,
-    screen.get_height() - 200,
+    (width - 300) / 2,
+    height - 200,
 )
-btn_roll_dice = pygame.Rect((screen.get_width() / 2) + 100, 50, 150, 75)
+btn_roll_dice = pygame.Rect((width / 2) + 100, 50, 150, 75)
+
+
+def draw_button(format, hover):
+    if hover:
+        pygame.draw.rect(screen, "skyblue2", format)
+        text = font.render("Roll Dice", True, "white")
+        text_rec = text.get_rect(center=(format.center))
+        screen.blit(text, text_rec)
+    else:
+        pygame.draw.rect(screen, "skyblue3", format)
+        text = font.render("Roll Dice", True, "white")
+        text_rec = text.get_rect(center=(format.center))
+        screen.blit(text, text_rec)
 
 
 while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    x, y = pygame.mouse.get_pos()
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray25")
 
     pygame.draw.rect(screen, "skyblue3", table_board)
     pygame.draw.rect(screen, "skyblue3", dice_board)
-    pygame.draw.rect(screen, "skyblue3", btn_roll_dice)
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_w]:
-    #     player_pos.y -= 300 * dt
-    # if keys[pygame.K_s]:
-    #     player_pos.y += 300 * dt
-    # if keys[pygame.K_a]:
-    #     player_pos.x -= 300 * dt
-    # if keys[pygame.K_d]:
-    #     player_pos.x += 300 * dt
+    draw_button(btn_roll_dice, hover=False)
+    if btn_roll_dice.collidepoint(x, y):
+        draw_button(btn_roll_dice, hover=True)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if btn_roll_dice.collidepoint(x, y):
+                print("button pressed!")
 
     # flip() the display to put your work on screen
     pygame.display.flip()
